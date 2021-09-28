833eae98a1589d8160df9ca04a7315131ccedf5e
adiguno <guodian44@gmail.com>
2021-09-27 22:53:47 -0400

    refactor simple agent

--- simple/agent.py
+++ simple/agent.py
@@ -25,42 +25,36 @@ def agent(observation, configuration):
 
     ### AI Code goes down here! ### 
     player = game_state.players[observation.player]
-    opponent = game_state.players[(observation.player + 1) % 2]
+    # opponent = game_state.players[(observation.player + 1) % 2]
     width, height = game_state.map.width, game_state.map.height
-
-    resource_tiles: list[Cell] = []
-    for y in range(height):
-        for x in range(width):
-            cell = game_state.map.get_cell(x, y)
-            if cell.has_resource():
-                resource_tiles.append(cell)
+    resource_tiles = get_resource_tiles(width, height)
 
     # we iterate over all our units and do something with them
     for unit in player.units:
-        if unit.is_worker() and unit.can_act():
-            closest_dist = math.inf
-            closest_resource_tile = None
-            if unit.get_cargo_space_left() > 0:
-                # if the unit is a worker and we have space in cargo, lets find the nearest resource tile and try to mine it
+        if is_movable_worker(unit):
+            if has_cargo_space(unit):
+                closest_dist = math.inf
+                closest_resource_tile = None
                 for resource_tile in resource_tiles:
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.COAL and not player.researched_coal(): continue
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.URANIUM and not player.researched_uranium(): continue
-                    dist = resource_tile.pos.distance_to(unit.pos)
-                    if dist < closest_dist:
-                        closest_dist = dist
-                        closest_resource_tile = resource_tile
+                    if is_wood_tile(resource_tile):
+                    # if resource_tile.resource.type == Constants.RESOURCE_TYPES.COAL and not player.researched_coal(): continue
+                        distance_to_worker = resource_tile.pos.distance_to(unit.pos)
+                        closest_resource_tile = update_closest_tile(closest_dist, resource_tile, distance_to_worker)
                 if closest_resource_tile is not None:
                     actions.append(unit.move(unit.pos.direction_to(closest_resource_tile.pos)))
             else:
+                # circle if worker is full
+                # actions.append(annotate.circle(unit.pos.x, unit.pos.y))
+
                 # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
                 if len(player.cities) > 0:
                     closest_dist = math.inf
                     closest_city_tile = None
                     for k, city in player.cities.items():
                         for city_tile in city.citytiles:
-                            dist = city_tile.pos.distance_to(unit.pos)
-                            if dist < closest_dist:
-                                closest_dist = dist
+                            distance_to_worker = city_tile.pos.distance_to(unit.pos)
+                            if distance_to_worker < closest_dist:
+                                closest_dist = distance_to_worker
                                 closest_city_tile = city_tile
                     if closest_city_tile is not None:
                         move_dir = unit.pos.direction_to(closest_city_tile.pos)
@@ -70,3 +64,28 @@ def agent(observation, configuration):
     # actions.append(annotate.circle(0, 0))
     
     return actions
+
+def get_resource_tiles(width, height):
+    resource_tiles: list[Cell] = []
+    for y in range(height):
+        for x in range(width):
+            cell = game_state.map.get_cell(x, y)
+            if cell.has_resource():
+                resource_tiles.append(cell)
+    return resource_tiles
+
+def update_closest_tile(closest_dist, resource_tile, distanceToWorker):
+    if distanceToWorker < closest_dist:
+        closest_dist = distanceToWorker
+        closest_resource_tile = resource_tile
+    return closest_resource_tile
+
+def is_wood_tile(resource_tile):
+    return resource_tile.resource.type == Constants.RESOURCE_TYPES.WOOD
+
+
+def is_movable_worker(unit):
+    return unit.is_worker() and unit.can_act()
+
+def has_cargo_space(unit):
+    return unit.get_cargo_space_left() > 0
